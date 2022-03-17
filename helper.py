7a7de9075c03aa8d98f63b77aecda09402bf1e9b
Shubham <tiwarishubham635@gmail.com>
2022-03-18 01:48:07 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -134,7 +134,7 @@ def realtime(val):
     predictions = []
     threshold = 0.5
 
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     # Set mediapipe model
     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
         while cap.isOpened():
